331109710b65e6675c89932c329cc6b6f31c52f5
alansuresh <69746521+alansuresh@users.noreply.github.com>
2022-03-28 10:39:24 +0530

    first

--- lib/presentation/main_page/screen_main_page.dart
+++ lib/presentation/main_page/screen_main_page.dart
@@ -1,22 +1,21 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:audire_application_1/core/colors/colors.dart';
-
 import 'package:audire_application_1/main.dart';
+
 import 'package:audire_application_1/presentation/audire/screen_audire.dart';
 import 'package:audire_application_1/presentation/downloads/screen_downloads.dart';
 import 'package:audire_application_1/presentation/home/screen_home.dart';
 import 'package:audire_application_1/presentation/Video/screen_videos.dart';
 import 'package:audire_application_1/presentation/main_page/widgets/bottom_nav.dart';
-
+import 'package:audire_application_1/drawer_item.dart';
 import 'package:flutter/material.dart';
 
 class ScreenMainPage extends StatelessWidget {
   ScreenMainPage({Key? key}) : super(key: key);
 
   final _pages = [
-    ScreenHome(),
     Home(),
+    ScreenHome(),
     Screenaudire(),
     screenvideos(),
     Screendownloads(),
